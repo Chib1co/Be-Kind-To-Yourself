64a3591d3bcbf35fc355729c9487f683a75d97e0
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-05-15 12:36:32 +1000

    file name update

---
+++ client/src/components/Logbtn/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import "./LoginBtn.css";
+
+
+export default function LoginBtn(props) {
+   
+        return (
+            <button onClick={props.handleRouteLogin}>Login</button>
+  
+        )
+    
+}

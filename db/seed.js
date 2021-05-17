5374e269f55868a96f96de43411e994dfb4fe885
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-05-18 09:49:59 +1000

    deploy debugging

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,15 @@
 const userSeed = require("./userSeeder");
 const activitySeed = require("./activitySeeder");
 const resultSeed = require("./resultSeeder");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+    process.env.MONGODB_URI ||"mongodb://localhost/Be-Kind-To-Yourself",
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    }
+  )
 
 
 function seeder(){

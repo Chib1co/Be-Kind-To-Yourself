703304c0149f19af0406676540e18f22cfa4538f
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-05-27 14:47:17 +1000

    debugging session

--- client/src/components/LoginCard/index.js
+++ client/src/components/LoginCard/index.js
@@ -1,7 +1,7 @@
 
 import React, {useState, useEffect } from "react";
 import "./LoginCard.css"
-import LoginBtn from "../LoginBtn/Index.js"
+// import LoginBtn from "../components/LoginBtn"
 
 
 export default function LoginCard(props) {
